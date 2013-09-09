c3787c6c4f3949c60a8d77b08a39181da6788383
Judson Neer <judson.neer@gmail.com>
2013-09-08 22:20:16 -0700

    Correct a logic error that incorrectly initializaed the HTTP server.

--- mixmaestro.py
+++ mixmaestro.py
@@ -14,14 +14,14 @@ import httpserver
 
 # Configure the logging module.
 _logformat = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
-logging.basicConfig(format=_logformat, level=logging.DEBUG)
+logging.basicConfig(format=_logformat, level=logging.INFO)
 
 
 # Initialize the mixer object depending on command line parameters.
 if len(sys.argv) > 2 and sys.argv[1] == 'RolandVMixer':
     import rolandvmixer
     _mixer = rolandvmixer.RolandVMixer(sys.argv[2])
-if len(sys.argv) > 2 and sys.argv[1] == 'TestRolandVMixer':
+elif len(sys.argv) > 2 and sys.argv[1] == 'TestRolandVMixer':
     import testrolandvmixer
     _mixer = testrolandvmixer.TestRolandVMixer(sys.argv[2])
 else:
