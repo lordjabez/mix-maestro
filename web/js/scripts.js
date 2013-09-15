760c7216c0810146f19781a28549123f0a89aaf1
Judson Neer <judson.neer@gmail.com>
2013-09-14 21:53:57 -0700

    Continued implementation of scripts.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,77 +3,97 @@
 
 "use strict";
 
-// TODO read this from JSON
-var chanPerPage = 8
-var currPage = 1
-var numPages = 6
-
-function updateInputs(inputs) {
-    for(var input in inputs) {
-        var name = inputs[input].name || "";
-        var inputElem = $('#chan-00');
-        if(name != "") {
-            inputElem.show();
-            inputElem.find('ind-name').html(name);
-        }
-        else {
-            chanElem.hide();
-            inputElem.find('ind-name').html('&nbsp;');
-        }
-        var auxes = inputs[input].auxes || {};
-        for(var aux in auxes) {
-            var level = (auxes[aux].level || "") + " dB" ;
-            inputElem.find('
-            $('body div:nth-child(' + aux + ') table td:nth-child(' + input + ') p:nth-child(2)').html(level);
-        }
-    }
-    setTimeout(pollInputs, 1000);
-}
-
-// TODO use query string to poll right value
-function pollInputs() {
-    $.get('/auxes/1/inputs', updateInputs);
-}
-
-function refreshPageInfo() {
-    $('#this-page .ui-btn-text').html(currPage + '/' + numPages);
-}
-
-function firstPage() {
-    currPage = 1;
-    refreshPageInfo();
-}
-
-function prevPage() {
-    currPage = Math.max(1, currPage - 1);
-    refreshPageInfo();
-}
-
-function nextPage() {
-    currPage = Math.min(currPage + 1, numPages);
-    refreshPageInfo();
-}
-
-function lastPage() {
-    currPage = numPages;
-    refreshPageInfo();
-}
-
 $(document).bind('pageinit', function() {
 
-    $('#first-page').bind('tap', firstPage);
-    $('#prev-page').bind('tap', prevPage);
-    $('#next-page').bind('tap', nextPage);
-    $('#last-page').bind('tap', lastPage);
+    var CHANNEL_POLL_DELAY = 1000;
+
+    var id = location.search.split('=')[1];
+
+    var stripsPerPage = 1;
+    var currPage = 1;
+    var numPages = 1;
+
+    var channel;
+
+    function updateStrip(snum) {
+        var inum = (currPage - 1) * stripsPerPage + snum + 1;
+        var name = channel['inputs'][inum].name || '';
+        var level = (channel['inputs'][inum].level.toFixed(1) || '') + " dB";
+        if(name) {
+            $(this).removeClass('ui-disabled');
+            $(this).find('.ind-name').html(name);
+            $(this).find('.ind-level').html(level);
+        }
+        else {
+            $(this).addClass('ui-disabled');
+            $(this).find('.ind-name').html('&nbsp;');
+            $(this).find('.ind-level').html('&nbsp;');
+        }
+    }
+
+    function updatePage() {
+        if(channel !== undefined) {
+            numPages = Math.ceil(Object.keys(channel['inputs']).length / stripsPerPage);
+            var pageHeader = channel.name + ' ' + currPage + '/' + numPages;
+            $('#button-this-page .ui-btn-text').html(pageHeader);
+            $('.chan-strip').each(updateStrip);
+        }
+    }
+
+    function updateChannel(chan) {
+        channel = chan;
+        updatePage();
+    }
+
+    function setNextPoll() {
+        setTimeout(pollChannel, CHANNEL_POLL_DELAY);
+    }
+
+    function pollChannel() {
+        var channelUrl = '/auxes/' + id + '/inputs'
+        $.ajax({url: channelUrl, success: updateChannel, complete: setNextPoll});
+    }
+
+    function firstPage(e) {
+        e.stopImmediatePropagation();
+        currPage = 1;
+        updatePage();
+    }
+
+    function prevPage(e) {
+        e.stopImmediatePropagation();
+        currPage = Math.max(1, currPage - 1);
+        updatePage();
+    }
+
+    function thisPage(e) {
+        e.stopImmediatePropagation();
+    }
+
+    function nextPage(e) {
+        e.stopImmediatePropagation();
+        currPage = Math.min(currPage + 1, numPages);
+        updatePage();
+    }
+
+    function lastPage(e) {
+        e.stopImmediatePropagation();
+        currPage = numPages;
+        updatePage();
+    }
+
+    stripsPerPage = $('.chan-strip').size();
+
+    $('#button-first-page').bind('tap', firstPage);
+    $('#button-prev-page').bind('tap', prevPage);
+    $('#button-this-page').bind('tap', thisPage);
+    $('#button-next-page').bind('tap', nextPage);
+    $('#button-last-page').bind('tap', lastPage);
     
-    refreshPageInfo();
-    
-    pollInputs();
+    updatePage();
+    pollChannel();
 
 /*
-
-    //TODO format string in display to 0.0 form
-
     $("#i1_up").click(function(e) {
         oldlevel = parseFloat($("#i1_level").html());
         newlevel = oldlevel + 1.0;
