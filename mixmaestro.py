b08a5eff569a69ea8ccbe1b2aaed2416e8c29c83
Judson Neer <judson.neer@gmail.com>
2013-09-27 08:46:03 -0700

    Add a default serial port so it doesn't need to be specified.

--- mixmaestro.py
+++ mixmaestro.py
@@ -15,7 +15,7 @@ import httpserver
 _parser = argparse.ArgumentParser()
 _parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='enable debug logging')
 _parser.add_argument('-m', '--mixer', default='GenericMixer', help='mixer module to use')
-_parser.add_argument('-p', '--port', help='serial port used by the mixer module')
+_parser.add_argument('-p', '--port', default='/dev/ttyAMA0', help='serial port used by the mixer module')
 _args = _parser.parse_args()
 
 # Configure the logging module.
@@ -26,9 +26,9 @@ logging.basicConfig(format=_logformat, level=_loglevel)
 # Initialize the mixer object depending on command line parameters.
 mixer = __import__(_args.mixer.lower())
 Mixer = getattr(mixer, _args.mixer)
-if _args.port:
+try:
     _mixer = Mixer(_args.port)
-else:
+except TypeError:
     _mixer = Mixer()
 
 # Start the application components
