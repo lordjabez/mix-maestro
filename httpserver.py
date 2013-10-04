24e2e8b3b408b227c825bd60e5b2bbb8ceaea837
Judson Neer <judson.neer@gmail.com>
2013-10-03 22:01:57 -0700

    Improve how HTTPServer stores the mixer information.

--- httpserver.py
+++ httpserver.py
@@ -14,53 +14,54 @@ import bottle
 # Increase the maximum body size allowed by bottle
 bottle.BaseRequest.MEMFILE_MAX = 1024 * 1024
 
-# Mixer object storage
-_mixer = {}
-
 # Regular expressions that define a legal channel number and list of numbers
 _numregex = '[0-9]+'
 _numsregex = '[0-9]+(,[0-9]+)*'
 
 
+class Mixer(object):
+    mixer = None
+
+
 @bottle.get('/mixer')
 def _getmixer():
-    return _mixer['mixer'].getmixer()
+    return Mixer.mixer.getmixer()
 
 
 @bottle.get('/inputs')
 @bottle.get('/inputs/<inums:re:{0}>'.format(_numsregex))
 def _getinputs(inums=None):
-    return _mixer['mixer'].getinputs(inums)
+    return Mixer.mixer.getinputs(inums)
 
 
 @bottle.get('/auxes')
 @bottle.get('/auxes/<anums:re:{0}>'.format(_numsregex))
 def _getinputs(anums=None):
-    return _mixer['mixer'].getauxes(anums)
+    return Mixer.mixer.getauxes(anums)
 
 
 @bottle.get('/auxes/<anum:re:{0}>/inputs'.format(_numregex))
 @bottle.get('/auxes/<anum:re:{0}>/inputs/<inums:re:{1}>'.format(_numregex, _numsregex))
 def _getauxesinputs(anum, inums=None):
-    return _mixer['mixer'].getauxinputs(anum, inums)
+    return Mixer.mixer.getauxinputs(anum, inums)
 
 
 @bottle.put('/inputs')
 @bottle.put('/inputs/<inums:re:{0}>'.format(_numsregex))
 def _putinputs(inums=None):
-    _mixer['mixer'].setinputs(inums, bottle.request.json)
+    Mixer.mixer.setinputs(inums, bottle.request.json)
 
 
 @bottle.put('/auxes')
 @bottle.put('/auxes/<anums:re:{0}>'.format(_numsregex))
 def _putauxes(anums=None):
-    _mixer['mixer'].setauxes(anums, bottle.request.json)
+    Mixer.mixer.setauxes(anums, bottle.request.json)
 
 
 @bottle.put('/auxes/<anum:re:{0}>/inputs'.format(_numregex))
 @bottle.put('/auxes/<anum:re:{0}>/inputs/<inums:re:{1}>'.format(_numregex, _numsregex))
 def _putauxesinputs(anum, inums=None):
-    _mixer['mixer'].setauxinputs(anum, inums, bottle.request.json)
+    Mixer.mixer.setauxinputs(anum, inums, bottle.request.json)
 
 
 @bottle.get('/')
@@ -74,6 +75,6 @@ def start(mixer):
     Initializes the module.
     @param mixer: The mixer object into which the interface functions will call.
     """
-    _mixer['mixer'] = mixer
+    Mixer.mixer = mixer
     kwargs = {'host': '0.0.0.0', 'port': 8080, 'debug': False, 'quiet': True}
     threading.Thread(target=bottle.run, kwargs=kwargs).start()
